578cfdd1551b9d7adf6a7f0652c83c9cef187a00
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2023-04-06 12:46:30 -0400

    style: remove consoles

--- src/services/createUserService.ts
+++ src/services/createUserService.ts
@@ -31,40 +31,31 @@ class CreateUserService {
     const validateCPF = new ValidateCPFService();
     const validateAge = new ValidateAgeService();
 
-    console.log("1");
     const addressInfo = await getCEPComplements.handle(cep);
-    console.log("2");
 
     const cpfValid = validateCPF.handle(cpf);
-    console.log("3");
 
     if (!cpfValid.valid) {
       throw new AppError("CPF is not valid!", 400);
     }
-    console.log("4");
+
     const ageEighteenOrAbove = validateAge.handle(birth);
-    console.log("5");
 
     if (!ageEighteenOrAbove) {
       throw new AppError("You need to be at least 18 years old!", 400);
     }
-    console.log("6");
 
     let userExists = await this.userRepository.findByCPF(cpfValid.cpf);
-    console.log("7");
 
     if (userExists) {
       throw new AppError("User with this CPF already exists!", 400);
     }
-    console.log("8");
 
     userExists = await this.userRepository.findByEmail(email);
-    console.log("9");
 
     if (userExists) {
       throw new AppError("User with this email already exists!", 400);
     }
-    console.log("10");
 
     const user = await this.userRepository.create({
       name,
@@ -80,6 +71,8 @@ class CreateUserService {
       locality: addressInfo.localidade,
       uf: addressInfo.uf,
     });
+
+    user.password = undefined;
 
     return user;
   }
