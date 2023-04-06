97103ba1a6c32bfaea67973ba3e9844003963c51
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2023-04-05 22:09:34 -0400

    feat: add use routes and error middleware

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,28 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import morgan from "morgan";
+import "./config/env";
+import { databaseConnect } from "./config/database";
+import AppError from "./errors/appError";
+import "express-async-errors";
+import routes from "./routes";
 
 export const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, _: NextFunction) => {
+    if (error instanceof AppError) {
+      return response
+        .status(error.statusCode)
+        .json({ status: "error", message: error.message });
+    }
+    return response
+      .status(500)
+      .json({ status: "error", message: "💥 Internal server error." });
+  }
+);
+
+databaseConnect();
