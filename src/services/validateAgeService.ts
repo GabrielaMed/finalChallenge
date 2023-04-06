4e4bc3c7c3468a3d6309eb3166c8a4270163cd4c
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2023-04-05 22:13:59 -0400

    feat: add validate age and cpf services

---
+++ src/services/validateAgeService.ts
@@ -0,0 +1,17 @@
+class ValidateAgeService {
+  handle(dateOfBirth: Date): boolean {
+    const today = new Date();
+    const birthDateObj = new Date(dateOfBirth);
+    let age = today.getFullYear() - birthDateObj.getFullYear();
+    const monthDiff = today.getMonth() - birthDateObj.getMonth();
+    if (
+      monthDiff < 0 ||
+      (monthDiff === 0 && today.getDate() < birthDateObj.getDate())
+    ) {
+      age--;
+    }
+    return true;
+  }
+}
+
+export default ValidateAgeService;
---
+++ src/services/validateCPFService.ts
@@ -0,0 +1,38 @@
+class ValidateCPFService {
+  handle(cpf: string) {
+    cpf = cpf.replace(/\D/g, "");
+    let sum: number;
+    let rest: number;
+    sum = 0;
+
+    const isAllSame = [...cpf].every((digit) => digit === cpf[0]);
+    if (isAllSame) {
+      return { valid: false, cpf };
+    }
+
+    for (let i = 1; i <= 9; i++) {
+      sum += parseInt(cpf.substring(i - 1, i)) * (11 - i);
+    }
+    rest = (sum * 10) % 11;
+
+    if (rest == 10 || rest == 11) {
+      rest = 0;
+    }
+    if (rest != parseInt(cpf.substring(9, 10))) return { valid: false, cpf };
+
+    sum = 0;
+    for (let i = 1; i <= 10; i++) {
+      sum += parseInt(cpf.substring(i - 1, i)) * (12 - i);
+    }
+    rest = (sum * 10) % 11;
+
+    if (rest == 10 || rest == 11) {
+      rest = 0;
+    }
+    if (rest != parseInt(cpf.substring(10, 11))) return { valid: false, cpf };
+
+    return { valid: true, cpf };
+  }
+}
+
+export default ValidateCPFService;
