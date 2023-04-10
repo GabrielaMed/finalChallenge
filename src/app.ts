d8fd4802b36fbd5c17d5b2918139e4d3639391c8
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2023-04-10 10:50:34 -0400

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,15 @@ app.use(express.json());
 app.use(routes);
 app.use(cors());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "3.87.184.140:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(
   (error: Error, request: Request, response: Response, _: NextFunction) => {
     if (error instanceof AppError) {
